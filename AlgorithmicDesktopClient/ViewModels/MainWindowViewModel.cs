cbdbc5a0ce4a267080e0b2bb403be3f4b3949f47
Abdulaziz <abdu3332099@gmail.com>
2021-12-08 22:29:29 +0500

    Добавьте файлы проекта.

---
+++ AlgorithmicDesktopClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmicDesktopClient.ViewModels.Base;
+
+namespace AlgorithmicDesktopClient.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Algorithmic";
+
+        public string Title
+        {
+            get => _Title;
+            /*set
+            {
+                //if (Equals(_Title, value)) return;
+                //_Title = value;
+                //OnPropertyChanged();
+
+                Set(ref _Title, value);
+            }*/
+            set => Set(ref _Title, value);
+        }
+    }
+}
