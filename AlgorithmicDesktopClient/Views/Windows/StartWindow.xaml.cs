9600547e68a22e26fa041dd9a2a7f0faa2039351
Abdulaziz <abdu3332099@gmail.com>
2021-12-09 00:21:05 +0500

    gg

---
+++ AlgorithmicDesktopClient/Views/Windows/StartWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AlgorithmicDesktopClient.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task animation = new Task(() =>
+            {
+                var degrees = 30;
+                while (true)
+                {
+                    RotateTransform rotateTransform = new RotateTransform(degrees);
+                    load.RenderTransform = rotateTransform;
+                    degrees *= 2;
+                    Task.Delay(1000);
+                }
+            });
+            animation.Start();
+        }
+    }
+}
